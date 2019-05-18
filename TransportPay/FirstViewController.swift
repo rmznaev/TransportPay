e7176044ebc86115f3df0143acdc1d0eac279d93
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-05-18 18:30:29 +0400

    Initial Commit

---
+++ TransportPay/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TransportPay
+//
+//  Created by Ramazan Abdullayev on 5/18/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TransportPay/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TransportPay
+//
+//  Created by Ramazan Abdullayev on 5/18/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
